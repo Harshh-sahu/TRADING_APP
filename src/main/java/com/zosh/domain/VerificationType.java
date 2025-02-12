7969bf6a17f14b5611cee39cb5213e8ed65c8cdf
harsh <harshsahu1143@gmail.com>
2025-02-12 22:39:06 +0530

    LETS CREATING A USER ENTITY

---
+++ src/main/java/com/zosh/domain/VerificationType.java
@@ -0,0 +1,9 @@
+package com.zosh.domain;
+
+public enum VerificationType {
+
+
+    MOBILE,
+    EMAIL
+
+}
---
+++ src/main/java/com/zosh/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.zosh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zosh.modal.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
